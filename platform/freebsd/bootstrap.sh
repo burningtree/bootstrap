c8e6d91800f3a5d3c205b9586d2f134628c61639
Jan Stránský <jan.stransky@arnal.cz>
2016-09-03 12:38:14 +0200

    Make more BSD friendly (6).

--- platform/freebsd/bootstrap.sh
+++ platform/freebsd/bootstrap.sh
@@ -1,1 +1,21 @@
-echo "null"
+#!/bin/sh
+
+PACKAGES="fish vim-lite git tmux wget curl apg aria2 mtr ncdu jq the_silver_searcher"
+
+# check sudo
+SUDO="/usr/local/bin/sudo"
+
+if [ ! -x $SUDO ]; then
+  echo "Installing sudo .."
+  while ! su -m root -c "pkg install sudo"; do :; done
+fi
+echo "Sudo: $SUDO"
+
+echo "Checking sudoers group .."
+WHEEL_STATUS=`cat /usr/local/etc/sudoers | egrep -o '^\%wheel ALL=\(ALL\) ALL'`
+if [ ! "$WHEEL_STATUS" ]; then
+  while ! su -m root -c "echo \"%wheel ALL=(ALL) ALL\" >> /usr/local/etc/sudoers"; do :; done
+fi
+
+echo "Installing core packages .."
+$SUDO pkg install $PACKAGES
